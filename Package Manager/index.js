276d212a581c50a9785774cd67a3a7ef0a0b79ab
Byte-Block <Sergio.Rajic@gmail.com>
2020-06-08 07:59:33 +0200

    Initial working version

--- Package Manager/index.js
+++ Package Manager/index.js
@@ -1,5 +1,26 @@
 var figlet = require('figlet');
- 
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  const figlety = figlet.textSync('Boo!', {    //Change the 'Boo!' string for another result
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  });
+  //res.end('Hello World');
+  res.end(figlety);
+}); //Working version :)!!!
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
 // figlet('Hello World!!', function(err, data) {
 //     if (err) {
 //         console.log('Something went wrong...');
@@ -22,8 +43,8 @@ var figlet = require('figlet');
 //     console.log(data);
 // });
 
-console.log(figlet.textSync('Boo!', {
-    font: 'Ghost',
-    horizontalLayout: 'default',
-    verticalLayout: 'default'
-}));
+// console.log(figlet.textSync('Boo!', {
+//     font: 'Ghost',
+//     horizontalLayout: 'default',
+//     verticalLayout: 'default'
+// }));
