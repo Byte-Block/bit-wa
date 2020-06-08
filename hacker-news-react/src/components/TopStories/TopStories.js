8ab7e17b890402a1b79bcbf006415b3297bb7640
Byte-Block <Sergio.Rajic@gmail.com>
2020-06-08 14:36:43 +0200

    Second commit

--- hacker-news-react/src/components/TopStories/TopStories.js
+++ hacker-news-react/src/components/TopStories/TopStories.js
@@ -1,12 +1,36 @@
 import React from 'react';
 
+import { fetchIDsTopStories } from '../fetchIDsTopStories/fetchIDsTopStories';
+import { fetchStoryByID } from '../fetchStroyByID/fetchStoryByID';
+
+
 class TopStories extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            data: [],
+            stories: []
         };
     }
 
-    
+    componentDidMount() {
+        fetchIDsTopStories()
+            .then(data => {
+                this.setState({ data: data }, () => {
+                    const requests = this.state.stories.map(story => fetchStoryByID(story));
+                    Promise.all(requests)
+                        .then(data => {
+                            this.setState({ stories: data });
+                        });
+                });
+            });
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
 }
--- hacker-news-react/src/components/fetchIDsTopStories/fetchIDsTopStories.js
+++ hacker-news-react/src/components/fetchIDsTopStories/fetchIDsTopStories.js
@@ -1,8 +1,9 @@
-const fetchIDsTopStories = (resolve) => {
-    fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+const fetchIDsTopStories = () => {
+   return fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        
     })
 }
+
+export { fetchIDsTopStories };
